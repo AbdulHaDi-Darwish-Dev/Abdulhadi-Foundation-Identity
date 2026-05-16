76ee432593071eb6c7b8ce5f4e4d758d5c680e3e
Abdulhadidarwish <267627653+AbdulHaDi-Darwish-Dev@users.noreply.github.com>
2026-05-16 17:15:52 +0300

    Build Unit of Work

---
+++ Abdulhadi.Foundation.Identity.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Shared.Core;
+
+namespace Abdulhadi.Foundation.Identity.Application.Abstractions.Persistence;
+
+public interface IUnitOfWork
+{
+    IRepository<TEntity> Repository<TEntity>() where TEntity : class;
+
+    Task<OutputResult<bool>> CommitAsync();
+}
--- Abdulhadi.Foundation.Identity.Infrastructure/Persistence/UnitOfWork/EfUnirOfWork.cs
+++ Abdulhadi.Foundation.Identity.Infrastructure/Persistence/UnitOfWork/EfUnirOfWork.cs
@@ -1,35 +1,32 @@
 ﻿using BuildingBlocks.Shared.Core;
+using Abdulhadi.Foundation.Identity.Application.Abstractions.Persistence;
+using Abdulhadi.Foundation.Identity.Infrastructure.Persistence.Repositories;
 
 namespace Abdulhadi.Foundation.Identity.Infrastructure.Persistence.UnitOfWork;
 
-public class EfUnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _context;
 
-    public EfUnitOfWork(AppDbContext context)
+    public UnitOfWork(AppDbContext context)
     {
         _context = context;
     }
 
+    public IRepository<TEntity> Repository<TEntity>() where TEntity : class
+        => new Repository<TEntity>(_context);
+
     public async Task<OutputResult<bool>> CommitAsync()
     {
-        using var transaction = await _context.Database.BeginTransactionAsync();
-
         try
         {
             await _context.SaveChangesAsync();
-            await transaction.CommitAsync();
 
             return OutputResult<bool>.Ok(true);
         }
         catch (Exception ex)
         {
-            await transaction.RollbackAsync();
-
-            var inner = ex.InnerException?.Message;
-            var full = ex.ToString();
-
-            throw new PersistenceException(full);
+            throw new PersistenceException(ex.Message);
         }
     }
 }
